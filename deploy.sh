c7b28c2f9cdb6202371cd24974fcdd0a12871676
ultron01 <p.vurm@outlook.cz>
2023-11-11 13:22:29 +0100

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
-vendor/bin/phpunit
 
 git add .
 (git commit -m "Depoly app to server") || true
 (git push) || true
 
-git push origin master
+git push origin main
 
-git checkout master
+git checkout main

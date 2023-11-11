add1f8145c2c105c8a95e11b1f94ca4780f82af5
ultron01 <p.vurm@outlook.cz>
2023-11-11 15:04:50 +0100

    hbs

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const express = require('express');
 const faker = require('faker');
 const app = express();
 const server = require('http').Server(app);
-// const io = require('socket.io')(server);
+const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
-//require('./socketserver.js')(io, app);
+require('./socketserver.js')(io, app);
 require('./globals.js');
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
